1a6169f26216abe325266c90213162af49aea224
Coder <105261700+BalencedVeil717@users.noreply.github.com>
2022-07-17 21:55:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+// TIME
+function updateTime(){
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+    var t_str = hours + ":" + minutes + " ";
+    if(hours > 11){
+        t_str += "PM";
+    } else {
+        t_str += "AM";
+    }
+    document.getElementById('p').innerHTML = t_str;
+}
+setInterval(updateTime, 1000);
+
+// ARITHMETIC 
 function add() {
     var x = document.getElementById("a").value;
     var y = document.getElementById("b").value;
@@ -23,3 +42,4 @@ function add() {
     var multit = parseInt(x) * parseInt(y);
     document.getElementById("screen1").innerHTML=multit;
   }
+  
